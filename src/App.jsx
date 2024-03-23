a79f6928491822f2ca7d68979abb527121e2da7a
Carlos Eduardo <carloseduardo.moura.contato@gmail.com>
2024-03-23 16:30:16 -0300

    Primeira etapa do projeto, feito o header

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { Header } from './components/Header';
+import { Post } from './Post';
+import './styles.css';
+export function App() {
+  return (
+   
+   <div>
+     <Header />
+    
+
+    <Post
+      author="Carlos Eduardo" 
+      content="teste sendo realizado"
+    />
+    </div>
+)
+
+
+}
+
