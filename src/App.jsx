dc93f380d7273a89eeae36f4c67aaef8e848e155
Carlos Eduardo <carloseduardo.moura.contato@gmail.com>
2024-04-06 01:17:04 -0300

    feito o Siderbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 
 import { Header } from './components/Header';
 import { Post } from './Post';
+import style from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 export function App() {
   return (
    
    <div>
      <Header />
-    
-
-    <Post
+    <div className= {style.wrapper}>
+      <Sidebar />
+      <main>
+      <Post
       author="Carlos Eduardo" 
-      content="teste sendo realizado"
+      content="conteudo"
     />
+  
+      </main>
+    </div>
+
+
     </div>
 )
 
