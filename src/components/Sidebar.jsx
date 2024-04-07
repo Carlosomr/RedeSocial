7cf1029497848ddc76541e349dbd2723e1613488
Carlos Eduardo <carloseduardo.moura.contato@gmail.com>
2024-04-07 09:32:45 -0300

    adicionado a foto de perfil e de capa

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css';
+import {PencilLine} from 'phosphor-react'
 export function Sidebar(){
     return (
         <aside className={styles.Sidebar}>
@@ -6,12 +7,16 @@ export function Sidebar(){
             src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
             
             <div className={styles.profile}>
+            <img className= {styles.avatar} 
+            src='https://avatars.githubusercontent.com/u/142277959?v=4'></img>
+
                 <strong>Carlos Eduardo</strong>
                 <span>Desenvolvedor web</span>
              </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine />
                     Editar seu perfil
                 </a>
             </footer>
