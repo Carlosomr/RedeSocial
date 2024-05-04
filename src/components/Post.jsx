87bdcb3636cf71620cfcf29044d6b626f5a59502
Carlos Eduardo <carloseduardo.moura.contato@gmail.com>
2024-05-04 01:31:44 -0300

    alterando a foto da publicacao

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,10 +6,10 @@ export function Post(){
         <article className= {styles.post}>
             <header>
                 <div className={styles.author}>
-                <img className={styles.avatar} src='https://github.com/Carlosomr.png'></img>
+                <img className={styles.avatar} src='https://github.com/orodrigogo.png'></img>
                 <div className={styles.authorInfo}>
-                    <strong>Carlos Eduardo</strong>
-                    <span>Desenvolvedor Web</span>
+                    <strong>Rodrigo</strong>
+                    <span>Desenvolvedor Mobile</span>
 
                 </div>
                 </div>
@@ -17,11 +17,11 @@ export function Post(){
                 <time title='07 de abril de 2024 ás 11:20' dataTime="2024-04-07 11:20:00">Publicado há 1h   </time>
             </header>
             <div className= {styles.content}>
-            <p>Fala galeraa 👋 </p>
+            <p>Eai pessoal👋 </p>
 
             <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-            <p><a href='#'>carlos.dev/doctorcare</a></p>
+            <p><a href='#'>rodrigo.dev/doctorcare</a></p>
 
             <p> 
                 <a href='#'>#novoprojeto</a>
